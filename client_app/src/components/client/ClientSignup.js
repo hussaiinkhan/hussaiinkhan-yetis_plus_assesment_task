6b32bcb4d8a52b90c2e5991bc89e509097a1f1db
Sherry <126199512+hussaiinkhan@users.noreply.github.com>
2024-06-26 16:43:47 +0300

    Update ClientSignup.js

--- client_app/src/components/client/ClientSignup.js
+++ client_app/src/components/client/ClientSignup.js
@@ -13,7 +13,7 @@ const SignupPage = () => {
   const handleClientSignup = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5001/api/customer/register', {
+      const response = await axios.post('https://yetisplusapi.onrender.com/api/customer/register', {
         name,
         surname,
         budget,
